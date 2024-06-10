cfb6ea3460da3ccaacdb1d77df3026804d90a8f4
truongtn123 <149347567+truongtn123@users.noreply.github.com>
2024-06-10 20:26:21 +0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        // target: 'http://localhost:8080/api',
-        target: 'https://ecommerce-app-7fb8.onrender.com/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  }
 })
